de6915fad64a94cbfe58d4ae26b3d65d53433780
Rüdiger zu Dohna <snackbox@sinntr.eu>
2010-10-22 15:57:50 +0000

    cleanup of dependencies; started to remove CDI and JavaEE 6

--- demo/chat-send/src/main/java/net/java/dev/messageapi/chat/ChatSendControl.java
+++ demo/chat-send/src/main/java/net/java/dev/messageapi/chat/ChatSendControl.java
@@ -1,23 +1,28 @@
 package net.java.dev.messageapi.chat;
 
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import net.java.dev.messageapi.ChatApi;
 
+import com.oneandone.consumer.messageapi.adapter.DefaultJmsConfigFactory;
+import com.oneandone.consumer.messageapi.adapter.JmsConfig;
+import com.oneandone.consumer.messageapi.adapter.xml.JmsXmlSenderFactory;
+
 @Named
 @Stateless
 public class ChatSendControl {
+    // params are factory, destination, user, password
+    private static final JmsConfig CONFIG = DefaultJmsConfigFactory.getJmsConfig("java:/JmsXA",
+            "Chat", "admin", "admin");
 
-	@Inject
-	private ChatApi chat;
+    private final ChatApi chat = JmsXmlSenderFactory.createProxy(ChatApi.class, CONFIG);
 
-	public String getMessage() {
-		return "enter your message here";
-	}
+    public String getMessage() {
+        return "enter your message here";
+    }
 
-	public void setMessage(String message) {
-		chat.send(message);
-	}
+    public void setMessage(String message) {
+        chat.send(message);
+    }
 }
