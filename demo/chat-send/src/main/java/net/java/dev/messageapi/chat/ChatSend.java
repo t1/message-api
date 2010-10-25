8289209b3d6b4e82876789755018393fdbebe327
Rüdiger zu Dohna <snackbox@sinntr.eu>
2010-10-25 07:50:06 +0000

    chat-send as only one simple Servlet... no CDI, no JSP

--- demo/chat-send/src/main/java/net/java/dev/messageapi/chat/ChatSend.java
+++ demo/chat-send/src/main/java/net/java/dev/messageapi/chat/ChatSend.java
@@ -1,38 +1,57 @@
 package net.java.dev.messageapi.chat;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.http.*;
 
 import net.java.dev.messageapi.ChatApi;
 
-import com.oneandone.consumer.messageapi.adapter.*;
+import com.oneandone.consumer.messageapi.adapter.DefaultJmsConfigFactory;
+import com.oneandone.consumer.messageapi.adapter.JmsConfig;
 import com.oneandone.consumer.messageapi.adapter.xml.JmsXmlSenderFactory;
 
 public class ChatSend extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	// params are factory, destination, user, password
-	private static final JmsConfig CONFIG = DefaultJmsConfigFactory.getJmsConfig("java:/JmsXA",
-			"topic/testTopic", "guest", "guest");
+    // params are factory, destination, user, password
+    private static final JmsConfig CONFIG = DefaultJmsConfigFactory.getJmsConfig("java:/JmsXA",
+            "topic/testTopic", "guest", "guest");
 
-	private final ChatApi chat = JmsXmlSenderFactory.createProxy(ChatApi.class, CONFIG);
+    private final ChatApi chat = JmsXmlSenderFactory.createProxy(ChatApi.class, CONFIG);
 
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-		String message = request.getParameter("message");
-		chat.send(message);
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        // we're not expecting any GET parameters!
+        response(response);
+    }
 
-		response.sendRedirect("index.jsp");
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        request(request);
+        response(response);
+    }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		processRequest(request, response);
-	}
+    private void request(HttpServletRequest request) {
+        String message = request.getParameter("message");
+        if (message != null && message.length() > 0) {
+            chat.send(message);
+        }
+    }
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		processRequest(request, response);
-	}
+    private void response(HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("  <head><title>Chat Send</title></head>");
+        out.println("  <body>");
+        out.println("    <h2>Please enter your message</h2>");
+        out.println("    <form method=\"post\" action=\"chat-send\"><p/>");
+        out.println("      <input type=\"text\" name=\"message\" size=\"50\"/><br/>");
+        out.println("      <input type=\"submit\" value=\"Send\" />");
+        out.println("    </form>");
+        out.println("  </body>");
+        out.println("</html>");
+    }
 }
