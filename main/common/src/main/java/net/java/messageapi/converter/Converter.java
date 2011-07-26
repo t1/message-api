52bc6c9180079467b8fd56644ead22a365b4b774
Rüdiger zu Dohna <snackbox@sinntr.eu>
2011-07-26 08:04:07 +0000

    added FIXME about joda dependency

--- main/common/src/main/java/net/java/messageapi/converter/Converter.java
+++ main/common/src/main/java/net/java/messageapi/converter/Converter.java
@@ -5,6 +5,8 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
  * Just rename {@link XmlAdapter} to <code>Converter</code> with the ValueType bound to String
+ * 
+ * FIXME this introduces a dependency to JodaTime, which should be optional!
  */
 @XmlSeeAlso({ JodaInstantConverter.class, JodaLocalDateConverter.class,
         StringToBooleanConverter.class })
