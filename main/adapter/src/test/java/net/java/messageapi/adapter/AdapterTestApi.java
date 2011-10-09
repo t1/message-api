aac23cbf7e97cf2b13585eb12aba6464d15e4886
Rüdiger zu Dohna <snackbox@sinntr.eu>
2011-10-09 17:30:04 +0000

    removed fluff

--- main/adapter/src/test/java/net/java/messageapi/adapter/AdapterTestApi.java
+++ main/adapter/src/test/java/net/java/messageapi/adapter/AdapterTestApi.java
@@ -2,7 +2,6 @@ package net.java.messageapi.adapter;
 
 import net.java.messageapi.MessageApi;
 
-//TODO Find out why Eclipse builds the SimpleMethod classes for the test code (Maven does not)
 @MessageApi
 public interface AdapterTestApi {
     void adapterMethod1();
