f559891e1b83c141a800494aef3f9944a50f74b4
Rüdiger zu Dohna <snackbox@sinntr.eu>
2011-12-02 10:15:31 +0000

    JmsProperty is not a Qualifier

--- main/annotations/src/main/java/net/java/messageapi/JmsProperty.java
+++ main/annotations/src/main/java/net/java/messageapi/JmsProperty.java
@@ -7,20 +7,15 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import javax.inject.Qualifier;
-
 /**
- * Place this parameter of a MessageApi method into the JMS message header
- * instead of the payload.
+ * Place this parameter of a MessageApi method into the JMS message header instead of the payload.
  * <p>
- * If you annotate some indirect field of such a parameter, it will be in the
- * header, too, but it will still be part of the payload. To remove it from the
- * payload, make that field transient or annotate it as
+ * If you annotate some indirect field of such a parameter, it will be in the header, too, but it will still be part of
+ * the payload. To remove it from the payload, make that field transient or annotate it as
  * {@link javax.xml.bind.annotation.XmlTransient XmlTransient}.
  */
-@Qualifier
 @Target({ PARAMETER, FIELD })
 @Retention(RUNTIME)
 public @interface JmsProperty {
-	// intentionally empty
+    // intentionally empty
 }
