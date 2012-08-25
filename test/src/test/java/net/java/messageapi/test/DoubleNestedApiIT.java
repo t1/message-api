d999eeb03fafbeab4b44d050d778024467ccb818
Rüdiger zu Dohna <snackbox@sinntr.eu>
2012-08-25 19:30:32 +0200

    split Double- from NestedApi to reduce interference

--- test/src/test/java/net/java/messageapi/test/DoubleNestedApiIT.java
+++ test/src/test/java/net/java/messageapi/test/DoubleNestedApiIT.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 
 import net.java.messageapi.JmsIncoming;
 import net.java.messageapi.MessageApi;
+import net.java.messageapi.test.DoubleNestedApiIT.DoubleNestedApiContainer.DoubleNestedApi;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -21,11 +22,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
-public class NestedApiIT {
+public class DoubleNestedApiIT {
     @Deployment(name = "test-mdb")
     public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class, NestedApiIT.class.getName() + ".war") //
-        .addClasses(NestedApi.class).addClass(NestedApi.class.getName() + "MDB") //
+        return ShrinkWrap.create(WebArchive.class, DoubleNestedApiIT.class.getName() + ".war") //
+        .addClasses(DoubleNestedApi.class).addClass(DoubleNestedApi.class.getName() + "MDB") //
         .addAsLibraries(
                 DependencyResolvers.use(MavenDependencyResolver.class) //
                 .artifacts("net.java.messageapi:adapter:2.0-SNAPSHOT", "net.java.messageapi:annotations:2.0-SNAPSHOT",
@@ -33,12 +34,6 @@ public class NestedApiIT {
                 .resolveAsFiles()) //
         .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml") //
         ;
-    }
-
-    @MessageApi
-    // @DestinationName("queue/test")
-    public interface NestedApi {
-        public void nestedCall();
     }
 
     public static String RESULT;
@@ -58,13 +53,21 @@ public class NestedApiIT {
         RESULT = null;
     }
 
+    public static class DoubleNestedApiContainer {
+        @MessageApi
+        // @DestinationName("queue/test")
+        public interface DoubleNestedApi {
+            void doubleNestedApiCall();
+        }
+    }
+
     @JmsIncoming
-    static class NestedApiImpl implements NestedApi {
+    public static class DoubleNestedApiImpl implements DoubleNestedApi {
         @Override
-        public void nestedCall() {
+        public void doubleNestedApiCall() {
             System.out.println("actually called... acquire in-semaphore");
             inAcquire();
-            NestedApiIT.RESULT = "nested-test";
+            DoubleNestedApiIT.RESULT = "double-test";
             System.out.println("release out-semaphore");
             out.release();
             System.out.println("call done");
@@ -73,19 +76,19 @@ public class NestedApiIT {
 
     @SuppressWarnings("all")
     @Inject
-    NestedApi sender;
+    private DoubleNestedApi doubleNestedApiSender;
 
     @Test
-    public void sendShouldBeAsynchronous() throws Exception {
+    public void shouldCallDoubleNestedApi() throws Exception {
         assertNull(RESULT);
         System.out.println("calling");
-        sender.nestedCall();
+        doubleNestedApiSender.doubleNestedApiCall();
         assertNull(RESULT);
         System.out.println("call sent... release in-semaphore");
         in.release();
         System.out.println("... acquire out-semaphore");
         out.acquire();
         System.out.println("out-semaphore acquired... finish test");
-        assertEquals("nested-test", RESULT);
+        assertEquals("double-test", RESULT);
     }
 }
