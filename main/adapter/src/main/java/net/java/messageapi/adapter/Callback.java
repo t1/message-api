974731910774bb6ce9501436fcf9cb3e02e39133
Rüdiger zu Dohna <snackbox@sinntr.eu>
2013-07-06 13:05:49 +0200

    experiments with callbacks

---
+++ main/adapter/src/main/java/net/java/messageapi/adapter/Callback.java
@@ -0,0 +1,172 @@
+package net.java.messageapi.adapter;
+
+import java.lang.reflect.*;
+import java.util.concurrent.*;
+
+import javax.ejb.*;
+import javax.inject.Inject;
+import javax.naming.*;
+
+import org.slf4j.*;
+
+/**
+ * Executes a method call asynchronously and invokes a callback method with the result. For example:
+ * 
+ * <pre>
+ * @AsynchronousService
+ * interface CustomerService {
+ *     long createCustomer(String first, String last);
+ * }
+ * 
+ * import static net.java.messageapi.adapter.Callback.*;
+ * 
+ * class Client {
+ *     @Inject
+ *     CustomerService service;
+ *     
+ *     void main() {
+ *         replyTo(this).customerCreated(service.createCustomer("Joe", "Doe"));
+ *     }
+ *     
+ *     void customerCreated(long newCustomerId) {
+ *         ...
+ *     }
+ * }
+ * </pre>
+ * 
+ * @todo AsynchronousService annotation and an according Producer
+ * @todo get rid of the replyTo type argument (requires dynamic class instead of a proxy)
+ */
+public class Callback {
+    private static final Logger log = LoggerFactory.getLogger(Callback.class);
+    private static final ThreadLocal<Callback> CALL_INFO = new ThreadLocal<Callback>();
+
+    /**
+     * @see http://stackoverflow.com/questions/13932083/jboss-java-ee-container-and-an-executorservice
+     */
+    @Stateless(name = "Executor")
+    public static class ExecutorBean implements Executor {
+        @Override
+        @Asynchronous
+        public void execute(Runnable command) {
+            command.run();
+        }
+    }
+
+    public static <T> T forService(final Object target, Class<T> type) {
+        InvocationHandler handler = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                printMethod("store call info", method, args);
+                storeCallbackInfo(target, method, args);
+                return 0L;
+            }
+
+            private void storeCallbackInfo(Object target, Method method, Object[] args) {
+                if (CALL_INFO.get() != null)
+                    throw new IllegalStateException("call_info already set");
+                Callback callback = new Callback(target, method, args);
+                CALL_INFO.set(callback);
+            }
+        };
+        return type.cast(Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(),
+                new Class<?>[] { type }, handler));
+    }
+
+    public static <T> T replyTo(final T target, Class<T> type) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        InvocationHandler handler = new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) {
+                final Callback callback = CALL_INFO.get();
+                CALL_INFO.set(null);
+                callback.invokeAndReplyTo(target, method);
+                return null; // this is never used... the actual reply is asynchronous
+            }
+        };
+
+        @SuppressWarnings("unchecked")
+        T proxy = (T) Proxy.newProxyInstance(classLoader, new Class<?>[] { type }, handler);
+        return proxy;
+    }
+
+    private static void printMethod(String prefix, Method method, Object[] args) {
+        if (!log.isDebugEnabled())
+            return;
+        StringBuilder info = new StringBuilder();
+        for (Object arg : args) {
+            if (info.length() > 0)
+                info.append(", ");
+            info.append(arg);
+        }
+        info.insert(0, "(");
+        info.insert(0, method.getName());
+        info.append(")");
+        log.debug("{} {}", prefix, info);
+    }
+
+    private final Object target;
+    private final Method method;
+    private final Object[] args;
+
+    private Callback(Object target, Method method, Object[] args) {
+        this.target = target;
+        this.method = method;
+        this.args = args;
+    }
+
+    @Inject
+    static Executor executor;
+
+    private void invokeAndReplyTo(final Object callbackTarget, final Method callbackMethod) {
+        executor().execute(new Runnable() {
+            @Override
+            public void run() {
+                Object result = invoke();
+                callback(callbackTarget, callbackMethod, result);
+            }
+        });
+    }
+
+    /**
+     * If the @Inject via the {@link ExecutorBean} doesn't work, we'll try to look it up in JNDI... and if that doesn't
+     * work either, we'll fall back to a cached thread pool (which should be okay, as we then seem to be outside
+     * JavaEE).
+     */
+    private Executor executor() {
+        if (executor == null)
+            executor = lookupExecutor();
+        return executor;
+    }
+
+    private Executor lookupExecutor() {
+        try {
+            return (Executor) new InitialContext().lookup("java:module/Executor");
+        } catch (NamingException e) {
+            return Executors.newCachedThreadPool();
+        }
+    }
+
+    private Object invoke() {
+        printMethod("invoke", method, args);
+        try {
+            return method.invoke(target, args);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("while calling " + method + " on " + target, e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException("while calling " + method + " on " + target, e);
+        }
+    }
+
+    private void callback(Object callbackTarget, Method callbackMethod, Object result) {
+        printMethod("reply", callbackMethod, new Object[] { result });
+        try {
+            callbackMethod.invoke(callbackTarget, result);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ main/adapter/src/test/java/net/java/messageapi/adapter/CallbackTest.java
@@ -0,0 +1,64 @@
+package net.java.messageapi.adapter;
+
+import static java.util.concurrent.TimeUnit.*;
+import static net.java.messageapi.adapter.Callback.*;
+import static org.junit.Assert.*;
+
+import java.util.concurrent.Semaphore;
+
+import org.junit.Test;
+
+public class CallbackTest implements CustomerServiceReply {
+
+    private static final Semaphore semaphoreIn = new Semaphore(0);
+    private static final Semaphore semaphoreOut = new Semaphore(0);
+
+    public interface CustomerService {
+        public long createCustomer(String first, String last);
+    }
+
+    public static CustomerService realService = new CustomerService() {
+        @Override
+        public long createCustomer(String first, String last) {
+            // wait for the main thread to release me
+            try {
+                assertTrue(semaphoreIn.tryAcquire(1, SECONDS));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return (first + last).hashCode();
+        }
+    };
+
+    private static final CustomerService service = forService(realService, CustomerService.class);
+
+    private long createdCustomerId;
+
+    @Test
+    public void shouldReplyAsynchronously() throws Exception {
+        replyTo(this, CustomerServiceReply.class).customerCreated(service.createCustomer("Joe", "Doe"));
+        semaphoreIn.release(); // let the service thread continue
+
+        assertTrue(semaphoreOut.tryAcquire(1, SECONDS)); // wait for the callback from the service
+        assertEquals("JoeDoe".hashCode(), createdCustomerId);
+    }
+
+    @Test
+    public void shouldWorkRepeatedly() throws Exception {
+        replyTo(this, CustomerServiceReply.class).customerCreated(service.createCustomer("Joey", "Doey"));
+        semaphoreIn.release(); // let the service thread continue
+
+        assertTrue(semaphoreOut.tryAcquire(1, SECONDS)); // wait for the callback from the service
+        assertEquals("JoeyDoey".hashCode(), createdCustomerId);
+    }
+
+    @Override
+    public void customerCreated(long createdCustomerId) {
+        this.createdCustomerId = createdCustomerId;
+        semaphoreOut.release(); // let the main thread continue
+    }
+}
+
+interface CustomerServiceReply {
+    public void customerCreated(long createdCustomerId);
+}
