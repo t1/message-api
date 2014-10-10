facce33137f5e50f99be8fc0ff79e4e9d65f2401
Rüdiger zu Dohna <snackbox@sinntr.eu>
2014-10-10 19:33:19 +0200

    use official (JMS 2.0) default connection factory JNDI name

--- annotations/src/main/java/net/java/messageapi/ConnectionFactoryName.java
+++ annotations/src/main/java/net/java/messageapi/ConnectionFactoryName.java
@@ -3,21 +3,20 @@ package net.java.messageapi;
 import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.*;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 import javax.inject.Qualifier;
 
 /**
- * The name of the {@link javax.jms.ConnectionFactory} to be used. This indirectly selects the JMS
- * provider, if there are multiple. If you don't annotate this name, the default is
- * "ConnectionFactory".
+ * The name of the {@link javax.jms.ConnectionFactory} to be used. This indirectly selects the JMS provider, if there
+ * are multiple. If you don't annotate this name, the default is defined in the Java EE 7 spec, chapter EE.5.20 to be
+ * <code>java:comp/DefaultJMSConnectionFactory</code>.
  */
 @Qualifier
-@Target({FIELD, PARAMETER})
+@Target({ FIELD, PARAMETER })
 @Retention(RUNTIME)
 public @interface ConnectionFactoryName {
-    public static final String DEFAULT = "ConnectionFactory";
+    public static final String DEFAULT = "java:comp/DefaultJMSConnectionFactory";
 
     public String value();
 }
